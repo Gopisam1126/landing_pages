20bdadd8d6a5c848c2529dc4ff87f376f6b842c0
scriptKid <gopikrishnan0045@gmail.com>
2024-10-15 12:38:00 +0530

    add acc - stat

--- Anime/src/assets/components/createArea.jsx
+++ Anime/src/assets/components/createArea.jsx
@@ -1,4 +1,5 @@
-import "../componentStyles/createArea.css"
+import "../componentStyles/createArea.css";
+import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
 function CreateArea() {
     return <>
         <section className="main-ca-sec">
@@ -10,6 +11,36 @@ function CreateArea() {
                     <p className="explore-p">
                         Dive into the <span lang="ja" className="ja-world-span"> 世界 </span> of <span className="anime-word-span">Anime</span> and <span className="manga-word-span"> 漫画</span>
                     </p>
+                    <div className="explore-btn-c">
+                        <button className="explore-btn">
+                            EXPLORE
+                            <div className="exp-icon-c">
+                                <ArrowOutwardIcon style={{
+                                    position: "relative",
+                                    top: "-0.4rem"
+                                }}/>
+                            </div>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className="current-status-container">
+                <div className="ep-ch-stat-c">
+                    <h3 className="completed-head">COMPLETED</h3>
+                    <div className="ep-ch-c">
+                        <div className="ep-stat">
+                            <span className="num-ep">120</span>
+                            <p className="completed-p">Episodes Completed</p>
+                        </div>
+                        <div className="continue-b-btn-c">
+                            <button className="continue-btn">
+                                CONTINUE WATCHING
+                                <div className="cw-icon-c">
+                                    <ArrowOutwardIcon/>
+                                </div>
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
